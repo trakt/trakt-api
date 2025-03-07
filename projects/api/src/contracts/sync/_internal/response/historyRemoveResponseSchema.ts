03de8b6d3dcd9c9cb4f5cb1768ca3462620031b1
seferturan <seferturan85@gmail.com>
2025-03-07 15:42:45 +0100

    feat(sync): add support for added history items

--- projects/api/src/contracts/sync/_internal/response/historyRemoveResponseSchema.ts
+++ projects/api/src/contracts/sync/_internal/response/historyRemoveResponseSchema.ts
@@ -1,7 +1,13 @@
 import { bulkMediaRequestSchema } from '../../../_internal/request/bulkMediaRequestSchema.ts';
 import { z } from '../../../_internal/z.ts';
 
+const historyChangeCountSchema = z.object({
+  movies: z.number(),
+  episodes: z.number(),
+});
+
 export const historyRemoveResponseSchema = z.object({
-  removed: z.object({ movies: z.number(), episodes: z.number() }),
+  added: historyChangeCountSchema,
+  removed: historyChangeCountSchema,
   not_found: bulkMediaRequestSchema,
 });
