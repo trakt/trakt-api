147502e9c990f3fde87301a3362794b66c450698
seferturan <seferturan85@gmail.com>
2025-03-14 14:15:41 +0100

    feat(comment): add support for liking and unliking comments

--- projects/api/src/contracts/comments/index.ts
+++ projects/api/src/contracts/comments/index.ts
@@ -1,7 +1,7 @@
 import { builder } from '../_internal/builder.ts';
 import { idParamsSchema } from '../_internal/request/idParamsSchema.ts';
 import { pageQuerySchema } from '../_internal/request/pageQuerySchema.ts';
-import type { z } from '../_internal/z.ts';
+import { z } from '../_internal/z.ts';
 import { commentLikeResponseSchema } from './_internal/responses/commentLikeResponseSchema.ts';
 
 export const comments = builder.router({
@@ -12,6 +12,24 @@ export const comments = builder.router({
     query: pageQuerySchema,
     responses: {
       200: commentLikeResponseSchema.array(),
+    },
+  },
+  like: {
+    path: '/like',
+    method: 'POST',
+    pathParams: idParamsSchema,
+    body: z.undefined(),
+    responses: {
+      204: z.undefined(),
+    },
+  },
+  unlike: {
+    path: '/like',
+    method: 'DELETE',
+    pathParams: idParamsSchema,
+    body: z.undefined(),
+    responses: {
+      204: z.undefined(),
     },
   },
 }, {
