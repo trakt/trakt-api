373eb62e9164f4e860dfaaaa069dbbdae9e56e9b
seferturan <seferturan85@gmail.com>
2025-03-14 12:29:09 +0100

    feat(user): add support for getting liked items

---
+++ projects/api/src/contracts/users/_internal/response/likedItemResponseSchema.ts
@@ -0,0 +1,81 @@
+import { commentResponseSchema } from '../../../_internal/response/commentResponseSchema.ts';
+import { episodeResponseSchema } from '../../../_internal/response/episodeResponseSchema.ts';
+import { listResponseSchema } from '../../../_internal/response/listResponseSchema.ts';
+import { movieResponseSchema } from '../../../_internal/response/movieResponseSchema.ts';
+import { showResponseSchema } from '../../../_internal/response/showResponseSchema.ts';
+import { z } from '../../../_internal/z.ts';
+import { seasonResponseSchema } from '../../../shows/_internal/response/seasonResponseSchema.ts';
+
+const likedMovieCommentResponseSchema = z.object({
+  /***
+   * Available if requesting extended `comments`.
+   */
+  comment_type: z.literal('movie'),
+  /***
+   * Available if requesting extended `comments`.
+   */
+  movie: movieResponseSchema.optional(),
+});
+
+const likedShowCommentResponseSchema = z.object({
+  /***
+   * Available if requesting extended `comments`.
+   */
+  comment_type: z.literal('show'),
+  /***
+   * Available if requesting extended `comments`.
+   */
+  show: showResponseSchema.optional(),
+});
+
+const likedSeasonCommentResponseSchema = z.object({
+  /***
+   * Available if requesting extended `comments`.
+   */
+  comment_type: z.literal('season'),
+  /***
+   * Available if requesting extended `comments`.
+   */
+  show: showResponseSchema.optional(),
+  /***
+   * Available if requesting extended `comments`.
+   */
+  season: seasonResponseSchema.optional(),
+});
+
+const likedEpisodeCommentResponseSchema = z.object({
+  /***
+   * Available if requesting extended `comments`.
+   */
+  comment_type: z.literal('episode'),
+  /***
+   * Available if requesting extended `comments`.
+   */
+  episode: episodeResponseSchema.optional(),
+});
+
+const likedExtendedCommentResponseSchema = z.union(
+  [
+    likedMovieCommentResponseSchema.partial(),
+    likedShowCommentResponseSchema.partial(),
+    likedEpisodeCommentResponseSchema.partial(),
+    likedSeasonCommentResponseSchema.partial(),
+  ],
+);
+
+const likedCommentResponseSchema = z.object({
+  liked_at: z.string(),
+  type: z.literal('comment'),
+  comment: commentResponseSchema,
+}).and(likedExtendedCommentResponseSchema);
+
+const likedListResponseSchema = z.object({
+  liked_at: z.string(),
+  type: z.literal('list'),
+  list: listResponseSchema,
+});
+
+export const likedItemResponseSchema = z.union([
+  likedCommentResponseSchema,
+  likedListResponseSchema,
+]);
