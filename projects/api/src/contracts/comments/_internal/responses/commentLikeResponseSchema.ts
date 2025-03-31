0bfb073cc8ae773795d39416ba5a145068ff1805
seferturan <seferturan85@gmail.com>
2025-04-01 00:06:09 +0200

    refactor: extract like response schema

--- projects/api/src/contracts/comments/_internal/responses/commentLikeResponseSchema.ts
+++
@@ -1,7 +0,0 @@
-import { profileResponseSchema } from '../../../_internal/response/userProfileResponseSchema.ts';
-import { z } from '../../../_internal/z.ts';
-
-export const commentLikeResponseSchema = z.object({
-  liked_at: z.string(),
-  user: profileResponseSchema,
-});
