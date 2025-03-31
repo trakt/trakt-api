e6027451d1ff76e7d0874cb0dc6d4249e7d1945f
seferturan <seferturan85@gmail.com>
2025-04-01 00:22:22 +0200

    feat(user): add support for getting list comments

---
+++ projects/api/src/contracts/users/_internal/request/listCommentsSortParamsSchema.ts
@@ -0,0 +1,10 @@
+import { z } from '../../../_internal/z.ts';
+
+export const listCommentsSortParamsSchema = z.object({
+  sort: z.enum([
+    'newest',
+    'oldest',
+    'likes',
+    'replies',
+  ]).optional(),
+});
