a3872d0d5ac511828bc9094099130b519d575d10
seferturan <seferturan85@gmail.com>
2025-04-03 10:44:14 +0200

    feat(list): add endpoints for getting popular and trending lists

--- projects/api/src/index.ts
+++ projects/api/src/index.ts
@@ -6,6 +6,7 @@ export type * from './contracts/calendars/index.ts';
 export type * from './contracts/certifications/index.ts';
 export type * from './contracts/comments/index.ts';
 export * from './contracts/enums/index.ts';
+export type * from './contracts/lists/index.ts';
 export type * from './contracts/movies/index.ts';
 export type * from './contracts/oauth/index.ts';
 export type * from './contracts/people/index.ts';
