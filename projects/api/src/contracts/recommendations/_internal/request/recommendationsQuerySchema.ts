5c468d545be69505325962a1c6ef0259afc012b0
Vlad Jerca <vlad.jerca@gmail.com>
2025-04-14 21:14:46 +0200

    feat(recommendation): update genre qp definition

--- projects/api/src/contracts/recommendations/_internal/request/recommendationsQuerySchema.ts
+++ projects/api/src/contracts/recommendations/_internal/request/recommendationsQuerySchema.ts
@@ -1,9 +1,8 @@
 import { ignoreQuerySchema } from '../../../_internal/request/ignoreQuerySchema.ts';
-import { genreEnumSchema } from '../../../_internal/response/genreEnumSchema.ts';
 import { z } from '../../../_internal/z.ts';
 
 export const recommendationsQuerySchema = z.object({
   limit: z.number().optional(),
   watch_window: z.number().optional(),
-  genres: z.array(genreEnumSchema).optional(),
+  genres: z.string().optional(),
 }).merge(ignoreQuerySchema);
