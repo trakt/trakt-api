55338dd65c7f5f55753b885471befa21cc9af0b1
seferturan <seferturan85@gmail.com>
2025-04-15 13:09:36 +0200

    feat(watchnow): add support for getting deep links

--- projects/api/src/contracts/episodes/index.ts
+++ projects/api/src/contracts/episodes/index.ts
@@ -1,10 +1,12 @@
 import { builder } from '../_internal/builder.ts';
+import { linksQuerySchema } from '../_internal/request/linksQuerySchema.ts';
 import { watchNowParamsSchema } from '../_internal/request/watchNowParamsSchema.ts';
 import { watchNowResponseSchema } from '../_internal/response/watchNowResponseSchema.ts';
 
 export const episodes = builder.router({
   watchnow: {
     path: '/watchnow/:country',
+    query: linksQuerySchema,
     method: 'GET',
     pathParams: watchNowParamsSchema,
     responses: {
