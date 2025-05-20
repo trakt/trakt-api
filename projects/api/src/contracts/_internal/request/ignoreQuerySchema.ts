02e78d6eab6d3b45126b7edf8962e5a66d24fcb5
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-20 15:36:02 +0200

    feat(api): extend zod with openapi information

--- projects/api/src/contracts/_internal/request/ignoreQuerySchema.ts
+++ projects/api/src/contracts/_internal/request/ignoreQuerySchema.ts
@@ -1,7 +1,13 @@
 import { z } from '../z.ts';
 
 export const ignoreQuerySchema = z.object({
-  ignore_watched: z.boolean().optional(),
-  ignore_collected: z.boolean().optional(),
-  ignore_watchlisted: z.boolean().optional(),
+  ignore_watched: z.boolean().optional().openapi({
+    description: 'Ignore watched items.',
+  }),
+  ignore_collected: z.boolean().optional().openapi({
+    description: 'Ignore collected items.',
+  }),
+  ignore_watchlisted: z.boolean().optional().openapi({
+    description: 'Ignore watchlisted items.',
+  }),
 });
