edbc258d2efa020a5a3852b1574df3e77ee574de
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-22 16:30:58 +0200

    feat: define operationId for swagger methods

--- projects/openapi/generate.ts
+++ projects/openapi/generate.ts
@@ -1,11 +1,27 @@
 import { traktContract } from '@trakt/api';
-import { generateOpenApi } from '@ts-rest/open-api';
+import { generateOpenApi } from './generateOpenApi.ts';
 
 export function generate(): ReturnType<typeof generateOpenApi> {
   return generateOpenApi(traktContract, {
     info: {
       title: 'Trakt API',
       version: '2.0.0',
+    },
+  }, {
+    operationMapper: (operation, route, id) => {
+      const parts = [
+        ...(operation.tags ?? []),
+        id,
+      ];
+
+      return {
+        ...operation,
+        operationId: `${route.method.toLowerCase()}${
+          parts
+            .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+            .join('')
+        }`,
+      };
     },
   });
 }
