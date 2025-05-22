6ecdd0e33f71caab44e1a78570a0c20dfa34e353
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-22 14:52:55 +0200

    fix: mark floating point numbers

--- projects/api/src/contracts/_internal/response/distributionResponseSchema.ts
+++ projects/api/src/contracts/_internal/response/distributionResponseSchema.ts
@@ -1,14 +1,44 @@
 import { z } from '../z.ts';
 
 export const distributionResponseSchema = z.object({
-  1: z.number().finite(),
-  2: z.number().finite(),
-  3: z.number().finite(),
-  4: z.number().finite(),
-  5: z.number().finite(),
-  6: z.number().finite(),
-  7: z.number().finite(),
-  8: z.number().finite(),
-  9: z.number().finite(),
-  10: z.number().finite(),
+  1: z.number().openapi({
+    type: 'number',
+    format: 'float',
+  }),
+  2: z.number().openapi({
+    type: 'number',
+    format: 'float',
+  }),
+  3: z.number().openapi({
+    type: 'number',
+    format: 'float',
+  }),
+  4: z.number().openapi({
+    type: 'number',
+    format: 'float',
+  }),
+  5: z.number().openapi({
+    type: 'number',
+    format: 'float',
+  }),
+  6: z.number().openapi({
+    type: 'number',
+    format: 'float',
+  }),
+  7: z.number().openapi({
+    type: 'number',
+    format: 'float',
+  }),
+  8: z.number().openapi({
+    type: 'number',
+    format: 'float',
+  }),
+  9: z.number().openapi({
+    type: 'number',
+    format: 'float',
+  }),
+  10: z.number().openapi({
+    type: 'number',
+    format: 'float',
+  }),
 });
