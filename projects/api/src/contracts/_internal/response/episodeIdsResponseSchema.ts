250f85e425530aa46978e809b196c2cb39ea6db6
Vlad Jerca <vlad.jerca@gmail.com>
2025-05-30 11:29:16 +0200

    fix: mark non trakt id's as optional

--- projects/api/src/contracts/_internal/response/episodeIdsResponseSchema.ts
+++ projects/api/src/contracts/_internal/response/episodeIdsResponseSchema.ts
@@ -3,9 +3,9 @@ import { streamingIdsResponseSchema } from './streamingIdsResponseSchema.ts';
 
 export const episodeIdsResponseSchema = z.object({
   trakt: z.number().int(),
-  tvdb: z.number().int(),
+  tvdb: z.number().int().optional(),
   imdb: z.string().nullable(),
-  tmdb: z.number().int(),
+  tmdb: z.number().int().optional(),
   /**
    * Available if requesting extended `streaming_ids`.
    */
