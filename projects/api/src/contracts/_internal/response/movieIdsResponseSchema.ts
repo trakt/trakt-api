7c57e9417c041a9709e3cafb663ddffcb08b11bc
Mike <michal.drabik0@gmail.com>
2025-05-30 16:11:23 +0200

    fix: make non-trakt ids optional for movie

--- projects/api/src/contracts/_internal/response/movieIdsResponseSchema.ts
+++ projects/api/src/contracts/_internal/response/movieIdsResponseSchema.ts
@@ -4,8 +4,8 @@ import { streamingIdsResponseSchema } from './streamingIdsResponseSchema.ts';
 export const movieIdsResponseSchema = z.object({
   trakt: z.number().int(),
   slug: z.string(),
-  imdb: z.string(),
-  tmdb: z.number().int(),
+  imdb: z.string().nullable(),
+  tmdb: z.number().int().optional(),
   /**
    * Available if requesting extended `streaming_ids`.
    */
