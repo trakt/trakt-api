87fed7be745eab66ba945f5e8abdeeaab47aeba7
Vlad Jerca <vlad.jerca@gmail.com>
2025-06-02 16:53:00 +0200

    fix: force string type on limit params with multiple types

--- projects/api/src/contracts/_internal/request/limitlessQuerySchema.ts
+++ projects/api/src/contracts/_internal/request/limitlessQuerySchema.ts
@@ -4,7 +4,10 @@ export const limitlessQuerySchema = z.object({
   limit: z.number()
     .int()
     .or(z.literal('all'))
-    .optional().openapi({
-      description: 'The number of items per page',
+    .optional()
+    .openapi({
+      description: `The number of items per page, can be a number or the value all`,
+      type: 'string',
+      oneOf: undefined
     }),
 });
