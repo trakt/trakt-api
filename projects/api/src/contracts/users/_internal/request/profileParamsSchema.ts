e636a51bfc72919fb03dbb2410febcdfece5b567
Michal <michal.drabik0@gmail.com>
2025-06-25 13:10:13 +0200

    fix: add possible nullish field and use strings for profile request param

--- projects/api/src/contracts/users/_internal/request/profileParamsSchema.ts
+++ projects/api/src/contracts/users/_internal/request/profileParamsSchema.ts
@@ -1,7 +1,7 @@
 import { z } from '../../../_internal/z.ts';
 
 export const profileParamsSchema = z.object({
-  id: z.enum(['me']).or(z.string()).openapi({
+  id: z.string().openapi({
     description:
       `The slug that identifies the user, or "me" for the authenticated user.`,
   }),
