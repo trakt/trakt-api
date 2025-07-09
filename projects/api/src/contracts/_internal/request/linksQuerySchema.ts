0696b2a70e04dc084db397b5f7981fca939364db
seferturan <seferturan85@gmail.com>
2025-07-09 10:49:32 +0200

    feat(watchnow): adds support for webos links

--- projects/api/src/contracts/_internal/request/linksQuerySchema.ts
+++ projects/api/src/contracts/_internal/request/linksQuerySchema.ts
@@ -2,5 +2,6 @@ import type { CombinationsFrom } from '../../../types/CombinationsFrom.ts';
 import { z } from '../z.ts';
 
 export const linksQuerySchema = z.object({
-  links: z.custom<CombinationsFrom<['tvos', 'direct', 'android']>>().nullish(),
+  links: z.custom<CombinationsFrom<['tvos', 'direct', 'android', 'webos']>>()
+    .nullish(),
 });
