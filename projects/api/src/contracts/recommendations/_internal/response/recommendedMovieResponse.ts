2a6b93ae3e534d0cc28278dbf2e1a5fb9441a46a
seferturan <seferturan85@gmail.com>
2025-07-11 15:38:33 +0200

    fix(recommendation): users can be null

--- projects/api/src/contracts/recommendations/_internal/response/recommendedMovieResponse.ts
+++ projects/api/src/contracts/recommendations/_internal/response/recommendedMovieResponse.ts
@@ -5,7 +5,7 @@ import { userProfileWithNotesSchema } from './userProfileWithNotesSchema.ts';
 export const recommendedMovieResponse = z.array(
   movieResponseSchema
     .extend({
-      favorited_by: z.array(userProfileWithNotesSchema),
-      recommended_by: z.array(userProfileWithNotesSchema),
+      favorited_by: z.array(userProfileWithNotesSchema).nullable(),
+      recommended_by: z.array(userProfileWithNotesSchema).nullable(),
     }),
 );
