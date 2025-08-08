32ab17174fdf70b95af5a5ff0561646c79b6864e
Michal <michal.drabik0@gmail.com>
2025-08-08 10:35:03 +0200

    feat(search): updated search definitions to please Kotlin client

--- projects/api/src/contracts/search/_internal/response/searchPersonResponseSchema.ts
+++ projects/api/src/contracts/search/_internal/response/searchPersonResponseSchema.ts
@@ -1,8 +1,8 @@
-import { z } from '../../../_internal/z.ts';
+import { int64, z } from '../../../_internal/z.ts';
 import { peopleSummaryResponseSchema } from '../../../people/_internal/response/peopleSummaryResponseSchema.ts';
 
 export const searchPersonResponseSchema = z.object({
-  score: z.number().int(),
+  score: int64(z.number().int()),
   type: z.literal('person'),
   person: peopleSummaryResponseSchema,
 });
