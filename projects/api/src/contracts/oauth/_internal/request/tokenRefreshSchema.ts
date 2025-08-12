ae37fe3b68fce535773b02cbe0b43f907575a4c8
Michal <michal.drabik0@gmail.com>
2025-08-12 17:43:01 +0200

    fix: make grant_type string not literal

--- projects/api/src/contracts/oauth/_internal/request/tokenRefreshSchema.ts
+++ projects/api/src/contracts/oauth/_internal/request/tokenRefreshSchema.ts
@@ -6,5 +6,8 @@ export const tokenRefreshSchema = tokenBaseSchema.extend({
     description:
       'The refresh token which was sent from the server during the exchange of the code.',
   }),
-  grant_type: z.literal('refresh_token'),
+  grant_type: z.string({
+    description:
+      'Defines how an access token is obtained.',
+  }),
 });
