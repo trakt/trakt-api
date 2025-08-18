2e8a6249bb646d7e74690466a6b0ae868a6fd088
Michal <michal.drabik0@gmail.com>
2025-08-18 11:00:03 +0200

    fix: make tmdb ID nullish

--- projects/api/src/contracts/_internal/response/seasonIdsResponseSchema.ts
+++ projects/api/src/contracts/_internal/response/seasonIdsResponseSchema.ts
@@ -3,5 +3,5 @@ import { z } from '../z.ts';
 export const seasonIdsResponseSchema = z.object({
   trakt: z.number().int(),
   tvdb: z.number().int().nullish(),
-  tmdb: z.number().int(),
+  tmdb: z.number().int().nullish(),
 });
