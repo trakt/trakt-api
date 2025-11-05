3444b9ff7392277163a0b425e7f42bda3a19c64b
seferturan <seferturan85@gmail.com>
2025-11-05 08:48:26 +0100

    fix(favorite): add missing definition for shows

--- projects/api/src/contracts/sync/schema/request/favoritesParamSchema.ts
+++ projects/api/src/contracts/sync/schema/request/favoritesParamSchema.ts
@@ -1,10 +1,14 @@
 import {
   movieIdsRequestSchema,
+  showIdsRequestSchema,
 } from '../../../_internal/request/idsRequestSchema.ts';
 import { z } from '../../../_internal/z.ts';
 
 export const favoriteParamSchema = z.object({
   movies: z.array(
     z.object({ ids: movieIdsRequestSchema }),
+  ).nullish(),
+  shows: z.array(
+    z.object({ ids: showIdsRequestSchema }),
   ).nullish(),
 });
