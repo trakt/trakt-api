fd0a76486ee97ab53de185dd964c8965c9826ca6
Mike <michal.drabik0@gmail.com>
2025-11-30 18:29:52 +0100

    fix(watchnow): remove extra ? in query

--- projects/api/src/contracts/_internal/request/countryParamsSchema.ts
+++ projects/api/src/contracts/_internal/request/countryParamsSchema.ts
@@ -1,7 +1,7 @@
 import { z } from '../z.ts';
 
 export const countryParamsSchema = z.object({
-  country: z.string().openapi({
+  country: z.string().optional().openapi({
     description: '2 character country code.',
   }),
 });
