854806809f681bc69b5ca0999893410487de7605
seferturan <seferturan85@gmail.com>
2026-04-21 13:04:42 +0200

    fix(sync): use float type for movie progress schema

--- projects/api/src/contracts/sync/schema/response/movieProgressResponseSchema.ts
+++ projects/api/src/contracts/sync/schema/response/movieProgressResponseSchema.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
-import { int64 } from '../../../_internal/z.ts';
+import { float, int64 } from '../../../_internal/z.ts';
 import { movieResponseSchema } from '../../../movies/index.ts';
 
 export const movieProgressResponseSchema = z.object({
-  progress: z.number().min(0).max(100),
+  progress: float(z.number().min(0).max(100)),
   paused_at: z.string().datetime(),
   id: int64(z.number().int()),
   type: z.literal('movie'),
